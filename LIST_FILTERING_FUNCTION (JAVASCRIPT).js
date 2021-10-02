cb91fad5e2eec1f9d6f3232c23758e71fd391241
SVENTRIPIKAL <90730468+SVENTRIPIKAL@users.noreply.github.com>
2021-10-02 16:09:17 -0500

    Add files via upload

---
+++ LIST_FILTERING_FUNCTION (JAVASCRIPT).js
@@ -0,0 +1,31 @@
+// LIST FILTERING
+// function receives a list
+// of positive integers and
+// strings, and returns the
+// same list with all strings
+// removed.
+
+function reomoveStrings( list )
+{
+    let newList = [] ;
+    
+    for ( let i = 0; i < list.length; i++ )
+    {
+        if ( typeof list[i] === 'string' )
+        {
+            continue;
+        }
+        
+        newList.push( list[i] ) ;
+    }
+    
+    return newList ;
+}
+
+
+//MINI-TEST
+let LIST = [ 'a', 'b', 'c', 1, 2, 3 ] ;
+
+let NEWLIST = reomoveStrings( LIST ) ;
+
+console.log( NEWLIST ) ;
