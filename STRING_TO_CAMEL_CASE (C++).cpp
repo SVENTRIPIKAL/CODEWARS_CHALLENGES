fca10391f077ca9554bcf9cd948a50efb3d5f4c0
SVENTRIPIKAL <90730468+SVENTRIPIKAL@users.noreply.github.com>
2021-10-10 23:18:52 -0500

    Add files via upload

---
+++ STRING_TO_CAMEL_CASE (C++).cpp
@@ -0,0 +1,49 @@
+/*  CONVERTING STRING TO CAMEL CASE
+*   
+*   function converts strings with dashes
+*   and underscores to camel case; the first
+*   word being capitalied only if initially capitalized.
+*   
+* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+#include <iostream>
+#include <string>
+using namespace std ;
+
+//=======================================================[ USER-DEFINED FUNCTIONS ]
+
+// #include "MyFunctions.h"     // imports functions from "MyFunctions.h" document
+
+string stringToCamel( string text )
+{
+    string solution = "" ;
+    
+    for ( unsigned int i = 0; i < text.size(); i++ )
+    {
+        if ( text[i] == '-' || text[i] == '_' )             { continue ; }
+        
+        if ( (i != 0) && (text[i-1] == '-' || text[i-1] == '_') )
+        {
+            solution.push_back( toupper( text[i] ) ) ;
+            
+            continue ;
+        }
+        
+        solution.push_back( text[i] ) ;
+    }
+    
+    return solution ;
+}
+
+//=========================================================================[ MAIN ]
+
+int main()
+{
+    string phrase = "string_is-now_camel-case" ;
+    
+    string solution = stringToCamel( phrase ) ;
+    
+    cout << solution << endl ;
+    
+    return 0 ;
+}
